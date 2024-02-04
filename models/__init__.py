f8d940e51a1c1c5995c5a34628fdf6142a906884
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-02-04 07:47:50 -0800

    done

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from os import getenv
+
+"""
+create the variable storage, an instance of FileStorage
+"""
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+storage.reload()
